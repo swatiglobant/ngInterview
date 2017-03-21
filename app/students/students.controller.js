141d788b038ca98351aa7a5c7725017e477b163a
swatiglobant <swati.jadhav@globant.com>
2017-03-21 09:44:43 +0530

    Implemented Add Student
    Created a form in student.html to add new student record.
    Added supported methods and implemented service call to add
    student by passing student oblect to it as input and again fetched
    all students from server.

--- app/students/students.controller.js
+++ app/students/students.controller.js
@@ -15,6 +15,8 @@
 			// Initialization code goes here
 			vm.Students = [];
 			vm.orderByValue = 'firstname';
+			vm.addStudentClick = false;
+			vm.student = {};
 			getStudents();
 		}
 
@@ -25,18 +27,40 @@
           vm.Students = data.data.student;
       });
 		}
+		vm.addStudentOnClick = function () {
+			vm.addStudentClick = true;
+			vm.student = {};
+		};
+
+		vm.addStudentOnClose = function () {
+			vm.addStudentClick = false;
+		};
 
 		vm.removeStudent = function (id) {
-			StudentsService.deleteStudent(id).then(function (data) {
+			StudentsService.deleteStudent(id).then(function (response) {
 				console.log("Student removed");
 			},
 			function (error) {
 				console.log("Error while processing request");
 			});
-		}
+		};
 
 		vm.setOrderByValue = function (value) {
 			vm.orderByValue = value;
-		}
+		};
+
+		vm.addStudent = function functionName() {
+			console.log(vm.student);
+			if ($scope.form.$valid) {
+				StudentsService.addStudent(vm.student).then(function (response) {
+					console.log("Student added successfully!!");
+				}, function (error) {
+					console.log("Error while processing request");
+				});
+				vm.student = {};
+				vm.addStudentClick = false;
+			}
+			activate();
+		};
 	}
 })();
