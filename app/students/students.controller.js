c79a24d968d215cbfa365c763df6efce8760ee98
swatiglobant <swati.jadhav@globant.com>
2017-03-21 08:45:35 +0530

    Implemented Show, delete apis along with SortbyValue & Search.
    By using the StudentService's getStudents and deleteStudent methods
    loaded data into view (Student.html) and displayed it in tabular form.
    Also provided facilities of Search and Sort using angular's filter &
    orderby.

--- app/students/students.controller.js
+++ app/students/students.controller.js
@@ -5,27 +5,38 @@
 		.module('ngInterview.students')
 		.controller('StudentsController', StudentsController);
 
-	StudentsController.$inject = [];
-	function StudentsController() {
-
-		/**
-		 * Model
-		 */
+	StudentsController.$inject = ['$scope','StudentsService'];
+	function StudentsController($scope, StudentsService) {
 
 		var vm = this;
-
-		/**
-		 * Initialization
-		 */
-
 		activate();
-
-		/**
-		 * Implementations
-		 */
 
 		function activate() {
 			// Initialization code goes here
+			vm.Students = [];
+			vm.orderByValue = 'firstname';
+			getStudents();
+		}
+
+		function getStudents() {
+      return StudentsService.getStudents()
+        .then(function(data) {
+					console.log(data);
+          vm.Students = data.data.student;
+      });
+		}
+
+		vm.removeStudent = function (id) {
+			StudentsService.deleteStudent(id).then(function (data) {
+				console.log("Student removed");
+			},
+			function (error) {
+				console.log("Error while processing request");
+			});
+		}
+
+		vm.setOrderByValue = function (value) {
+			vm.orderByValue = value;
 		}
 	}
 })();
