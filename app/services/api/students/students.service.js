3433b94168d40886cd98d701b531abd2390057a5
swatiglobant <swati.jadhav@globant.com>
2017-03-21 09:41:54 +0530

    Method To Add Student
    Included method addStudent in StudentService to process
    post requset to add new student record.

--- app/services/api/students/students.service.js
+++ app/services/api/students/students.service.js
@@ -10,13 +10,14 @@
 
 		this.getStudents = getStudents;
 		this.deleteStudent = deleteStudent;
+		this.addStudent = addStudent;
 
 		function getStudents() {
       var q = $q.defer();
       var request = $http({
         method: 'GET',
         // url: 'http://localhost:5000'+'/students',
-        url: 'http://demo1634462.mockable.io'+'/students',
+        url: 'http://demo1634462.mockable.io'+'/students'
       });
 			q.resolve(request);
 			return q.promise;
@@ -27,7 +28,19 @@
 			var request = $http({
 				method: 'DELETE',
 				// url: 'http://localhost:5000'+'/students',
-				url: 'http://demo1634462.mockable.io'+'/students/'+ studentId,
+				url: 'http://demo1634462.mockable.io'+'/students/'+ studentId
+			});
+			q.resolve(request);
+			return q.promise;
+		}
+
+		function addStudent(data) {
+			var q = $q.defer();
+			var request = $http({
+				method: 'POST',
+				// url: 'http://localhost:5000'+'/students',
+				url: 'http://demo1634462.mockable.io'+'/students/',
+				data: { student: data }
 			});
 			q.resolve(request);
 			return q.promise;
